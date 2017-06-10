f4524b3debd77e7903679bca14a0160d693e00c8
Justin O'Neill <Justin@Oneezy.localdomain>
2017-06-10 16:31:25 -0500

    initializing

---
+++ sw-precache-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  staticFileGlobs: [
+    '/index.html',
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/images/*'
+  ],
+  navigateFallback: '/index.html',
+  navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/).*/],
+  runtimeCaching: [
+    {
+      urlPattern: /\/data\/images\/.*/,
+      // handler: 'cacheFirst',
+      handler: 'networkFirst',
+      options: {
+        cache: {
+          maxEntries: 200,
+          name: 'items-cache'
+        }
+      }
+    },
+    {
+      urlPattern: /\/data\/.*json/,
+      // handler: 'fastest',
+      handler: 'networkFirst',
+      options: {
+        cache: {
+          maxEntries: 100,
+          name: 'data-cache'
+        }
+      }
+    }
+  ]
+};
